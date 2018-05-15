beaf5084789b67549d2877392448338d34d26573
dstoianov <denys.stoianov@gmail.com>
2018-05-16 01:04:58 +0200

    add cpu temperature

--- examples/send_to_thingspeak.py
+++ examples/send_to_thingspeak.py
@@ -7,6 +7,8 @@ Data is displayed at https://thingspeak.com/channels/324453/
 """
 from __future__ import print_function
 
+import os
+
 import urllib2
 
 import Adafruit_DHT
@@ -27,14 +29,21 @@ def read_dht_sensor():
     return round(hum, 1), round(temp, 1)
 
 
+# Get CPU temperature using 'vcgencmd measure_temp'
+def get_cpu_temp():
+    temp = os.popen('vcgencmd measure_temp').readline()
+    return temp.replace("temp=", "").replace("'C\n", "")
+
+
 def main():
     baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI
     try:
         hum, temp1 = read_dht_sensor()
+        temp2 = get_cpu_temp()
 
-        print("t={}\nh={}".format(temp1, hum))
+        print("t1={}\nt2={}\nh={}".format(temp1, temp2, hum))
 
-        f = urllib2.urlopen(baseURL + "&field1=%s&field2=%s" % (hum, temp1))
+        f = urllib2.urlopen(baseURL + "&field1=%s&field2=%s&field3=%s" % (hum, temp1, temp2))
         f.close()
     except Exception as e:
         print('Holy Cow, something Broke!!! exiting.' + str(e))
